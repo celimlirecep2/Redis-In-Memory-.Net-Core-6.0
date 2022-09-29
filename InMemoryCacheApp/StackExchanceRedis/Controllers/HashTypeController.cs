1f243edff8abc5843ad51524ea093783b642684c
Recep Çelimli <recep.celimli@outlook.com>
2022-09-29 23:03:27 +0300

    redis-finish

---
+++ InMemoryCacheApp/StackExchanceRedis/Controllers/HashTypeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchanceRedis.Services;
+using StackExchange.Redis;
+
+namespace StackExchanceRedis.Controllers
+{
+    public class HashTypeController : Controller
+    {
+        private readonly IDatabase db;
+        private string hashkey = "sözlük";
+        public HashTypeController(RedisCacheService redis)
+        {
+            db = redis.GetDb(4);
+        }
+        public IActionResult Index()
+        {
+            Dictionary<string,string> list = new Dictionary<string, string>();
+
+            if (db.KeyExists(hashkey))
+            {
+                db.HashGetAll(hashkey).ToList().ForEach(i =>
+                {
+                    list.Add(i.Name.ToString(),i.Value.ToString());
+                });
+
+                //db.HashGet(hashkey, "pen"); tek bir değer almak için kullanırız
+            }
+
+
+            return View(list);
+        }
+        [HttpPost]
+        public IActionResult Add(string key, string value)
+        {
+
+            db.HashSet(hashkey, key, value);
+          
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(string key)
+        {
+            bool result = await db.HashDeleteAsync(hashkey,key);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ InMemoryCacheApp/StackExchanceRedis/Controllers/SortedController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchanceRedis.Services;
+using StackExchange.Redis;
+
+namespace StackExchanceRedis.Controllers
+{
+    public class SortedController : Controller
+    {
+        //score değeri belirleyerek sıralama ayarı yapıyoruz
+        /// <summary>
+        /// mehmet 10
+        /// veli 2
+        /// recep1
+        /// sıralama recep veli a-mehmet
+        /// aynı scoreda olabilirler fakat aynı isimde olamazlar 
+        /// aynı isimde farklı skor değeri ile gelinirse skor değerini güncellemeye alır  
+        /// </summary>
+
+        private readonly IDatabase db;
+        private string listKey = "sortedName";
+        public SortedController(RedisCacheService redis)
+        {
+            db = redis.GetDb(3);
+        }
+        public IActionResult Index()
+        {
+            HashSet<string> list = new HashSet<string>();
+
+            if (db.KeyExists(listKey))
+            {
+                db.SortedSetScan(listKey).ToList().ForEach(i =>
+                {
+                    list.Add(i.Element.ToString());
+                });
+
+              var buyuktenKucuge=  db.SortedSetRangeByRank(listKey, order: Order.Descending).ToList();
+                var aralık = db.SortedSetRangeByRank(listKey,0,5, order: Order.Descending).ToList();
+            }
+
+          
+            return View(list);
+        }
+        [HttpPost]
+        public IActionResult Add(string name,int score)
+        {
+         
+            db.SortedSetAdd(listKey,name,score);
+            db.KeyExpire(listKey, DateTime.Now.AddMinutes(5));
+            return RedirectToAction(nameof(Index));
+        }
+
+        public  IActionResult Delete(string name)
+        {
+            bool result= db.SortedSetRemove(listKey, name.Split(":")[0]);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
